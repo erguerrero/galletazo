d209272979184f96275758ce5fb38e132773b571
erguerrero <errguerrero@gmail.com>
2014-11-24 20:03:38 -0600

    Agregar estructura inicial

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,38 @@
+var App = function() {
+
+	var _counter = 0;
+	var _loop = function() {
+
+		$('.cookieCounter').text(_counter + ' ' + 'Galletas');
+		$('.cookie').css({
+				opacity: 1
+			});
+
+		setTimeout(_loop, 100);
+	};
+
+	this.init = function() {
+
+
+		var clicker = $('.cookie').click(function() {
+
+			_counter++;
+			$('.cookie').css({
+				opacity: 0.7
+			})
+
+		});
+
+		_loop();
+	};
+}
+
 $(document).ready(function() {
-  'use strict';
-  console.log('Hola mundo');
+
+	var app = new App();
+	app.init();
+	
 });
+
+
+
